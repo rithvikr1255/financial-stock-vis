47af7544a5a1062b205e935c83d5ba8fc2b2ffcf
Rithvik Reddygari <67663680+ricky122-5@users.noreply.github.com>
2024-05-15 22:11:01 -0400

    Initial commit

---
+++ stockvis/src/constants/config.js
@@ -0,0 +1,6 @@
+export const chartConfig = {
+    "1D" : {days: 1, weeks: 0, months: 0, years: 0, resolution: "D"},
+    "1W" : {days: 0, weeks: 1, months: 0, years: 0, resolution: "W"},
+    "1M" : {days: 0, weeks: 0, months: 1, years: 0, resolution: "M"},
+    "1Y" : {days: 0, weeks: 0, months: 0, years: 1, resolution: "Y"},
+}
